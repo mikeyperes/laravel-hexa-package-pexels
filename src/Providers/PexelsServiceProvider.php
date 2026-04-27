056e03d9f728082088597b9442dad24b71f408a0
mikeyperes <mikeyperes@users.noreply.github.com>
2026-04-27 03:29:39 +0000

    feat(pexels): register package metadata

--- src/Providers/PexelsServiceProvider.php
+++ src/Providers/PexelsServiceProvider.php
@@ -42,6 +42,13 @@ class PexelsServiceProvider extends ServiceProvider
             // HWS-SIDEBAR-MENU-3L-END
 
             $registry->registerSidebarLink('pexels.index', 'Pexels', 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z', 'Sandbox', 'pexels', 83);
+            $registry->registerPackage('pexels', 'hexawebsystems/laravel-hexa-package-pexels', [
+                'title' => 'Pexels',
+                'description' => 'Stock image search and import tooling powered by the Pexels API.',
+                'icon' => 'M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z',
+                'color' => 'amber',
+                'settingsRoute' => 'pexels.index',
+            ]);
         }
     }
 }
